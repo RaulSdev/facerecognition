17a73e3867e9a0b73d03a960803914bd6761e0a2
raulsdev <raulsdev@gmail.com>
2020-08-14 12:59:05 -0500

    implemented API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
 
+
+
+const app = new Clarifai.App({
+  apiKey: '6fbb75564c6d461080e39e5ccdb3883f'
+ });
 
 const particlesOptions ={
   particles:{
@@ -21,6 +28,33 @@ const particlesOptions ={
 
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  function onInputChange(event) {
+    setInput(event.target.value);
+}
+
+function onButtonSubmit(){
+  
+  // API for face Detection
+  app.models.predict(
+    Clarifai.FACE_DETECT_MODEL,
+    // useState for setImageUrl
+    setImageUrl(input)
+      )
+      .then(
+function(response) {
+  // do something with response
+  console.log(response);
+},
+function(err) {
+  // there was an error
+}
+);
+}
+
   return (
     <div className="App">
       <Particles className='particles'
@@ -29,10 +63,8 @@ function App() {
       <Navigation />
       <Logo / >
       <Rank />
-      < ImageLinkForm / >
-  {     /* 
-     
-      <FaceRecognition />*/}
+      <ImageLinkForm value={input} onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} /> 
+      <FaceRecognition imageUrl={imageUrl}  />
     </div>
   );
 }
